b08f824ba2ed72141af030323da8f1eb2dd5f543
CFerrino11 <39447693+CFerrino11@users.noreply.github.com>
2018-05-30 18:33:10 -0700

    Update Connection.java

--- Connection.java
+++ Connection.java
@@ -1,9 +1,40 @@
-/*
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.URL;
+
+/**
  *
- * @author IST 411 - Group 3 (Christopher Ferrino, Arlan Hamilton, Raymond Han, Cynthia Hilgeman)
+ * @author Chris
  */
- 
- public class Connection
+ public class app
  {
- 
+       public static void main(String[] args) 
+       {
+             String urlString;
+             urlString = "https://www.facebook.com";
+     //   InetAddress theAddress = inetAddress.getByName(new URL(urlString).getHost());
+         try 
+         {
+             InetAddress theAddress = InetAddress.getByName(new URL(urlString).getHost());
+             System.out.println(theAddress.getAddress());
+             System.out.println(theAddress.getHostName());
+             System.out.println(theAddress.getHostAddress());
+             System.out.println(theAddress.getCanonicalHostName());
+             System.out.println(theAddress.isReachable(5000));
+             System.out.println(theAddress.hashCode());
+       }
+       catch(Exception e)
+       {
+  
+       e.printStackTrace();
+
+       }
+  
+ }
+       
  }
